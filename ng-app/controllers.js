4e73bf5689d98508a44123d6343f807c7249f1cc
dinoopspark <dinoop@sparksupport.com>
2017-03-28 11:55:10 +0530

    updated

--- ng-app/controllers.js
+++ ng-app/controllers.js
@@ -2,14 +2,14 @@ app.controller('mainFormController', function ($scope, $window, appData, WebServ
 
     $scope.isDev = appEnv.isDev;
     $scope.appForm = {};
-    
+
     $scope.dataCoating = appData.coating;
     $scope.dataStock = appData.stock;
     $scope.dataColor = appData.color;
     $scope.dataRunsize = appData.runsize;
     $scope.stock = appData.stock[0].key;
     $scope.color = appData.color[0].key;
-    
+
     WebService.getData(appConst.apiRequest, {action: 'get_disabled_dates'}).success(function (response) {
 //        console.log(response.data.disabled_dates);
         $scope.disabled_dates = response.data.disabled_dates;
@@ -24,14 +24,6 @@ app.controller('mainFormController', function ($scope, $window, appData, WebServ
     }
 
     $scope.onChangeSize = function () {
-        
-        
-        $scope.product_chosen = $scope.stock + '-' + $scope.coating + '-' + $scope.size;
-        var productChosen = appData.products.getObj('value', $scope.product_chosen).data;
-
-        if (productChosen != null) {
-            $scope.product_chosen_uuid = appData.products.getObj('value', $scope.product_chosen).data.key;
-        }
 
         $scope.dataRunsize = appData.runsize;
         if ($scope.size == '8.5X7') {
@@ -39,8 +31,24 @@ app.controller('mainFormController', function ($scope, $window, appData, WebServ
         }
 
     }
+
     $scope.buttonLoader = false;
     $scope.onSubmitForm = function () {
+        // Find product UUID
+        if ($scope.coating == 'EDPCMATT' && $scope.size == '4.5X12') {
+            $scope.name_coating = 'PCMATT';
+        } else {
+            $scope.name_coating = $scope.coating;
+        }
+
+        $scope.product_chosen = $scope.stock + '-' + $scope.name_coating + '-' + $scope.size;
+        var productChosen = appData.products.getObj('value', $scope.product_chosen).data;
+
+        if (productChosen != null) {
+            $scope.product_chosen_uuid = productChosen.key;
+        }
+        
+        // Redirect to map
         $scope.buttonLoader = true;
         WebService.getData(appConst.apiRequest, {action: 'get_product_option', id: $scope.product_chosen_uuid}).success(function (response) {
             var param = "product=" + $scope.product_chosen_uuid + "&target_in_mailbox_date=" + $scope.appForm.mail_delivery_date + "&size=" + response.data.size + "&stock=" + response.data.stock + "&coating=" + response.data.coating + "&color=" + $scope.color + "&runsize=" + $scope.runsize + "&job_name=test&phone=&address=";
